3d9640cb87318211e6179ff67048f8fdaede17b0
vladislavponomaryov96 <vladislavponomaryov96@gmail.com>
2023-03-25 21:09:52 +0600

    Start writing main page content layout controller

---
+++ src/components/contentLayoutController/index.js
@@ -0,0 +1,83 @@
+(function () {
+    let cl = console.log
+
+    let mainContainer = document.querySelector('main .content')
+    let mainContainerStyle = getComputedStyle(mainContainer)
+    let mainContainerWidth = clearPX(mainContainerStyle.getPropertyValue('width'))
+    let currentCW, largeCW = 1700, desktopCW = 1400, mediumCW = 982, smallCW = 600, extraSmallCW = 599.95
+
+    function clearPX(element) {
+        return +element.slice(0, element.length - 2)
+    }
+
+    new ResizeObserver(entries => {
+        // We wrap it in requestAnimationFrame to avoid this error - ResizeObserver loop limit exceeded
+        window.requestAnimationFrame(() => {
+            if (!Array.isArray(entries) || !entries.length) {
+                return;
+            }
+            getParams()
+            controll()
+        });
+    }).observe(mainContainer);
+
+    function getParams() {
+        mainContainerStyle = getComputedStyle(mainContainer)
+        mainContainerWidth = clearPX(mainContainerStyle.getPropertyValue('width'))
+    }
+
+    function addStyles(styles) {
+        let id = 'mainContentStyles'
+
+        if (document.getElementById(id)) {
+            let stylesElement = document.getElementById(id)
+            stylesElement.innerHTML = styles
+
+        } else {
+            let stylesElement = document.createElement('style')
+            stylesElement.id = id
+            document.head.append(stylesElement)
+
+            stylesElement.innerHTML = styles
+        }
+
+        cl('work')
+    }
+
+    function checkCurrentPosition(gettingCW,width) {
+        if (currentCW !== gettingCW) {
+            currentCW = gettingCW
+
+            addStyles(`main .content section {flex: 1 0 calc(${width} - 16px - 0.01px)}`)
+        }
+    }
+
+    function controll() {
+
+        cl(mainContainerWidth)
+
+        switch (true) {
+            case mainContainerWidth > largeCW: {
+                checkCurrentPosition(largeCW,'20%')
+                break;
+            }
+            case mainContainerWidth > desktopCW: {
+                checkCurrentPosition(desktopCW,'25%')
+                break;
+            }
+            case mainContainerWidth > mediumCW: {
+                checkCurrentPosition(mediumCW,'33%')
+                break;
+            }
+            case mainContainerWidth > smallCW: {
+                checkCurrentPosition(smallCW,'50%')
+                break;
+            }
+            case mainContainerWidth < extraSmallCW: {
+                checkCurrentPosition(extraSmallCW,'100%')
+                break;
+            }
+        }
+    }
+
+})()
