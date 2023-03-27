0325cc8703b7eda067bb1bdf727e62423ea5c61e
vladislavponomaryov96 <vladislavponomaryov96@gmail.com>
2023-03-27 12:19:24 +0600

    Mobile header scroll hidden

---
+++ src/components/hiddenHeader/index.js
@@ -0,0 +1,20 @@
+(function () {
+    /* Когда пользователь прокручивает вниз, скрыть навигационную панель. Когда пользователь прокручивает вверх, показать навигационную панель */
+    let prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function () {
+
+        if (window.innerWidth < 792) {
+
+            var currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                document.querySelector("body > header").style.top = "0";
+            } else {
+                document.querySelector("body > header").style.top = "-56px";
+            }
+            prevScrollpos = currentScrollPos;
+
+        }
+    }
+
+})()
