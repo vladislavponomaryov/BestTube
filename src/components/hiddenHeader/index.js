1749ef3a51fb84cdfdadf3360a737cbf1c978820
vladislavponomaryov96 <vladislavponomaryov96@gmail.com>
2023-03-30 12:24:32 +0600

    Refactor

--- src/components/hiddenHeader/index.js
+++ src/components/hiddenHeader/index.js
@@ -1,13 +1,11 @@
 (function () {
 
-    if (document.body.classList[0] === 'video') return
-
     /* Когда пользователь прокручивает вниз, скрыть навигационную панель. Когда пользователь прокручивает вверх, показать навигационную панель */
     let prevScrollpos = window.pageYOffset;
 
     window.onscroll = function () {
 
-        if (window.innerWidth < 792) {
+        if (window.innerWidth < 792 && document.body.classList[0] !== 'video') {
 
             var currentScrollPos = window.pageYOffset;
             if (prevScrollpos > currentScrollPos) {
