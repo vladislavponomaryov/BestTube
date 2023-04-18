ae07b859776be24798f41db24b1929b2b092b3de
vladislavponomaryov96 <vladislavponomaryov96@gmail.com>
2023-04-18 20:07:04 +0600

    Add hooks

---
+++ src/hooks/useDrawer.js
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+export const useDrawer = () => {
+
+    //const {mainDrawer,setMainDrawer} = useState(true)
+    //const {openDrawer,setOpenDrawer} = useState(null)
+
+    return {
+        /*mainDrawer,
+        openDrawer*/
+    }
+}
